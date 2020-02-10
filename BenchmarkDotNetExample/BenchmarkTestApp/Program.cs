bac5ab01ab52595d496e97e716500533cd094fad
Martin <foeller.martin@googlemail.com>
2020-02-10 10:44:22 +0100

    filled example program with code

--- BenchmarkDotNetExample/BenchmarkTestApp/Program.cs
+++ BenchmarkDotNetExample/BenchmarkTestApp/Program.cs
@@ -1,11 +1,38 @@
-﻿using System;
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNetExample;
+using System;
 
 namespace BenchmarkTestApp
 {
-    class Program
+   //[SimpleJob(launchCount:3, warmupCount:10,targetCount:30)]
+   [SimpleJob(BenchmarkDotNet.Engines.RunStrategy.ColdStart, targetCount:5)]
+    [RPlotExporter]
+    public class Program
     {
+        private LongRunningClass _longRunningClass = new LongRunningClass();
+
+        [Benchmark]
+        public void DoMoveIn()
+        {
+            _longRunningClass.TrackIn("123");
+        }
+
+        [Benchmark]
+        public void DoMoveOut()
+        {
+            _longRunningClass.TrackOut("123");
+        }
+        
+        [Benchmark]
+        public void GetItem()
+        {
+             _longRunningClass.GetItem("123");
+        }
+
         static void Main(string[] args)
         {
+            var summary = BenchmarkRunner.Run<Program>();
 
         }
     }

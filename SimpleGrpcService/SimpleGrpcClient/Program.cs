ab364c950dbec0ed9e4355183a9189898687d680
Martin <foeller.martin@googlemail.com>
2020-04-16 11:33:39 +0200

    added grpc client

---
+++ SimpleGrpcService/SimpleGrpcClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleGrpcClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
